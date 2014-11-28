ef742134af305d6fa796b2b104889aff86eacda8
GengZhongFei <m@mmatoMacBook-Pro.local>
2014-11-28 16:59:34 +0800

    项目提交

---
+++ RadioCheckBox/RadioCheckBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RadioCheckBox
+//
+//  Created by GengZhongFei on 14/11/28.
+//  Copyright (c) 2014年 耿中飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RadioCheckBox/RadioCheckBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RadioCheckBox
+//
+//  Created by GengZhongFei on 14/11/28.
+//  Copyright (c) 2014年 耿中飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
